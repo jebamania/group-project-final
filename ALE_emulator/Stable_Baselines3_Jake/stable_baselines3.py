76da51aaef047edf6eb494f21f7c34798f06cdb7
jebamania <jebenuity@gmail.com>
2024-08-26 20:40:20 -0600

    separating different approaches into new files

--- ALE_emulator/Stable_Baselines3_Jake/stable_baselines3.py
+++ ALE_emulator/Stable_Baselines3_Jake/stable_baselines3.py
@@ -3,11 +3,8 @@
 #!pip install stable-baselines3
 #!pip install opencv-python
 
-import torch
 from ale_py import ALEInterface, roms
 from stable_baselines3.common.evaluation import evaluate_policy
-# from stable_baselines3.common.atari_wrappers import WarpFrame
-print(torch.cuda.is_available())  # Should return True if a GPU is available
 
 
 ale = ALEInterface()
@@ -15,7 +12,7 @@ ale.loadROM(roms.Tetris)
 ale.reset_game()
 
 #action_space = [1,2,3,4]  # 0: NOOP, 1: FIRE, 2: RIGHT, 3: LEFT, 4; DOWN
-# reward = ale.act(0)  # noop
+reward = ale.act(0)  # noop
 
 import gymnasium as gym
 import ale_py
@@ -23,12 +20,26 @@ import time
 
 gym.register_envs(ale_py)  # unnecessary but helpful for IDEs
 
-# Create the Tetris environment
+# Initialize parameters
 #obs_type="rgb"
 #obs_type="grayscale"
 obs_type="ram"
-#ale_model = 'Tetris-v5'
-ale_model = 'Tetris-ram-v5'
+
+ale_model = 'Tetris-v5'
+if obs_type == "ram":
+    ale_model = 'Tetris-ram-v5'
+
+new_model = True
+agentName = 'dqn_tetris'
+
+policy = 'MlpPolicy'
+if obs_type == "rgb":
+    policy = 'CnnPolicy' # only works with obs_type="rgb"
+
+total_timesteps = 440000000
+
+## Create the Tetris environment
+
 env = gym.make(f'ALE/{ale_model}'
     , obs_type = obs_type
     , frameskip = 4
@@ -36,20 +47,11 @@ env = gym.make(f'ALE/{ale_model}'
     # , noop_max = 30
 ) #, render_mode="human" # remove render_mode in training
 
-# env = WarpFrame(env, width=84, height=84)
-
 episodes = 5
 
-####### use stable_baselines3
+####### Create or Load Model
 
 from stable_baselines3 import DQN
-
-new_model = True
-
-agentName = 'dqn_tetris'
-policy = 'MlpPolicy'
-#policy = 'CnnPolicy' # only works with obs_type="rgb"
-total_timesteps = 440000000
 
 modelName = f"{agentName}_{policy}_{total_timesteps}_obs_{obs_type}"
 
@@ -113,37 +115,3 @@ print(f"Evaluation Complete: mean_reward: {mean_reward}, std_reward: {std_reward
 if new_model:
     model.save(modelName)
     del model # remove to demonstrate saving and loading
-
-
-
-# end stable_baselines3
-
-''' custom agent attempt
-from agent_smith import DQNAgentSmith
-agentSmith = DQNAgentSmith(env.get_state_size())
-obs = env.reset()
-done = False
-
-
-for episode in range(episodes):
-    idx = 0
-    while not done:
-        idx += 1
-        start_time = time.time()
-
-        # Use a random action for demonstration purposes
-        action = env.action_space.sample()
-        # next_states = env.get_next_states()
-        # best_state = agentSmith.best_state(next_states.values())
-
-        obs, reward, terminated, truncated, info = env.step(action)
-
-        if terminated or truncated:
-            done = True
-
-        # Add a small delay to slow down the game for human observation
-        time.sleep(0.001)
-
-
-env.close()
-'''
